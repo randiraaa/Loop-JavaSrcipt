c47454618d0096f92f4f719cc21aafd20c450ee2
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-01 22:29:42 +0700

    Exercise - LOOP No. 1

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('------------------------------')
+console.log('Exercise - LOOP No. 1')
+console.log('------------------------------')
+
+for (let user = 1; user <= 100; user++) {
+    console.log('User Ke-' + user)
+}
