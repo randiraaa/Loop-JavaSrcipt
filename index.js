8c5f1f0434d663d859c88d1309cf144124e18196
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-01 22:31:09 +0700

    Exercise - LOOP No. 2

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ console.log('------------------------------')
 for (let user = 1; user <= 100; user++) {
     console.log('User Ke-' + user)
 }
+
+console.log('------------------------------')
+console.log('Exercise - LOOP No. 2')
+console.log('------------------------------')
+
+let nilaiAwal = 0;
+
+for (let i = 1; i <= 10; i++) {
+  nilaiAwal += 2;
+  console.log(`Hasil penambahan ke-${i}: ${nilaiAwal}`);
+}
